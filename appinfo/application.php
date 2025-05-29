09c7aef01ded7bc7e310b2173ee64b945e8e6b8f
kDick91 <kdickson91@gmail.com>
2025-05-29 15:54:04 -0500

    attempting to move nav to top bar

--- appinfo/application.php
+++ appinfo/application.php
@@ -6,12 +6,10 @@ use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\BackgroundJob\IJobList;
-use OCP\Settings\IManager as ISettingsManager;
 use Psr\Log\LoggerInterface;
 use OCA\MemoriesAlerts\Service\AlertService;
 use OCA\MemoriesAlerts\BackgroundJob\SendDailyAlerts;
 use OCA\MemoriesAlerts\Controller\SettingsController;
-use OCA\MemoriesAlerts\Settings\SettingsProvider;
 
 class Application extends App implements IBootstrap {
     public const APP_ID = 'memories_alerts';
@@ -54,22 +52,10 @@ class Application extends App implements IBootstrap {
                 $c->getServer()->get(LoggerInterface::class)
             );
         });
-
-        // Register the SettingsProvider
-        $context->registerService(SettingsProvider::class, function ($c) {
-            return new SettingsProvider(
-                $c->getServer()->getURLGenerator(),
-                $c->getServer()->getL10N(self::APP_ID)
-            );
-        });
     }
 
     public function boot(IBootContext $context): void {
         $jobList = $context->getServerContainer()->get(IJobList::class);
         $jobList->add(SendDailyAlerts::class);
-
-        // Register the settings section
-        $settingsManager = $context->getServerContainer()->get(ISettingsManager::class);
-        $settingsManager->registerSection('personal', SettingsProvider::class);
     }
 }
